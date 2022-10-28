865c483a031d767f3c360529ca65b4bb4a3054de
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-28 22:32:39 +0200

    Nix Flake

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,19 +1,27 @@
 final:
-previous:
+prev:
+with final.lib;
 with final.haskell.lib;
 {
-  dirforestPackages =
-    let dirforestPkg = name:
-      (buildStrictly (final.haskellPackages.callCabal2nixWithOptions name (final.gitignoreSource (../. + "/${name}")) "--no-hpack" { }));
-    in
-    final.lib.genAttrs [
-      "dirforest"
-      "genvalidity-dirforest"
-    ]
-      dirforestPkg;
-  haskellPackages = previous.haskellPackages.override (old: {
-    overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
-      self: super: final.dirforestPackages
+  dirforestRelease =
+    final.symlinkJoin {
+      name = "dirforest-release";
+      paths = attrValues final.haskellPackages.dirforestPackages;
+    };
+
+  haskellPackages = prev.haskellPackages.override (old: {
+    overrides = composeExtensions (old.overrides or (_: _: { })) (
+      self: super:
+        let dirforestPackages =
+          let dirforestPkg = name:
+            (buildStrictly (final.haskellPackages.callCabal2nixWithOptions name ((../. + "/${name}")) "--no-hpack" { }));
+          in
+          final.lib.genAttrs [
+            "dirforest"
+            "genvalidity-dirforest"
+          ]
+            dirforestPkg;
+        in { inherit dirforestPackages; } // dirforestPackages
     );
   });
 }
