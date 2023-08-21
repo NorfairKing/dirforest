a2f160b424f15cea1888d30cc07f02daf75630d9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-08-21 14:38:25 +0200

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
-    flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     validity.url = "github:NorfairKing/validity";
     validity.flake = false;
@@ -27,75 +26,73 @@
     , nixpkgs-22_11
     , nixpkgs-22_05
     , nixpkgs-21_11
-    , flake-utils
     , pre-commit-hooks
     , validity
     , autodocodec
     , safe-coloured-text
     , sydtest
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-      let
-        pkgsFor = nixpkgs: import nixpkgs {
-          inherit system;
-          overlays = [
-            self.overlays.${system}
-            (import (validity + "/nix/overlay.nix"))
-            (import (autodocodec + "/nix/overlay.nix"))
-            (import (safe-coloured-text + "/nix/overlay.nix"))
-            (import (sydtest + "/nix/overlay.nix"))
-          ];
-        };
-        pkgs = pkgsFor nixpkgs;
-      in
-      {
-        overlays = import ./nix/overlay.nix;
-        packages.default = pkgs.dirforestRelease;
-        checks =
-          let
-            backwardCompatibilityCheckFor = nixpkgs:
-              let pkgs' = pkgsFor nixpkgs;
-              in pkgs'.dirforestRelease;
-            allNixpkgs = {
-              inherit
-                nixpkgs-22_11
-                nixpkgs-22_05
-                nixpkgs-21_11;
-            };
-            backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
-          in
-          backwardCompatibilityChecks // {
-            pre-commit = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                hlint.enable = true;
-                hpack.enable = true;
-                ormolu.enable = true;
-                nixpkgs-fmt.enable = true;
-                nixpkgs-fmt.excludes = [ ".*/default.nix" ];
-                cabal2nix.enable = true;
-              };
+    let
+      system = "x86_64-linux";
+      pkgsFor = nixpkgs: import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.${system}
+          (import (validity + "/nix/overlay.nix"))
+          (import (autodocodec + "/nix/overlay.nix"))
+          (import (safe-coloured-text + "/nix/overlay.nix"))
+          (import (sydtest + "/nix/overlay.nix"))
+        ];
+      };
+      pkgs = pkgsFor nixpkgs;
+    in
+    {
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system}.default = pkgs.dirforestRelease;
+      checks.${system} =
+        let
+          backwardCompatibilityCheckFor = nixpkgs:
+            let pkgs' = pkgsFor nixpkgs;
+            in pkgs'.dirforestRelease;
+          allNixpkgs = {
+            inherit
+              nixpkgs-22_11
+              nixpkgs-22_05
+              nixpkgs-21_11;
+          };
+          backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
+        in
+        backwardCompatibilityChecks // {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              hlint.enable = true;
+              hpack.enable = true;
+              ormolu.enable = true;
+              nixpkgs-fmt.enable = true;
+              nixpkgs-fmt.excludes = [ ".*/default.nix" ];
+              cabal2nix.enable = true;
             };
           };
-        devShells.default = pkgs.haskellPackages.shellFor {
-          name = "dirforest-shell";
-          packages = p: (builtins.attrValues p.dirforestPackages);
-          withHoogle = true;
-          doBenchmark = true;
-          buildInputs = with pkgs; [
-            niv
-            zlib
-            cabal-install
-          ] ++ (with pre-commit-hooks.packages.${system};
-            [
-              hlint
-              hpack
-              nixpkgs-fmt
-              ormolu
-              cabal2nix
-            ]);
-          shellHook = self.checks.${system}.pre-commit.shellHook;
         };
-      });
+      devShells.${system}.default = pkgs.haskellPackages.shellFor {
+        name = "dirforest-shell";
+        packages = p: (builtins.attrValues p.dirforestPackages);
+        withHoogle = true;
+        doBenchmark = true;
+        buildInputs = with pkgs; [
+          niv
+          zlib
+          cabal-install
+        ] ++ (with pre-commit-hooks.packages.${system};
+          [
+            hlint
+            hpack
+            nixpkgs-fmt
+            ormolu
+            cabal2nix
+          ]);
+        shellHook = self.checks.${system}.pre-commit.shellHook;
+      };
+    };
 }
